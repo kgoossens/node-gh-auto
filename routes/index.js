1f0c8d3dd3764bde43809271789d4f75b0146dfe
Kristof Goossens <kristof@gitlab.com>
2022-01-25 10:42:39 +0100

    Added !

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'the other side👋' });
+  res.render('index', { title: 'the other side👋!' });
 });
 
 module.exports = router;
